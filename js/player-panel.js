8064faeb4dd73142bcb6ba32b01a167196154171
Minzer-Alexey <minzer.alexey@gamil.com>
2015-02-12 00:04:20 +0200

    add kill count

--- js/player-panel.js
+++ js/player-panel.js
@@ -1,19 +1,20 @@
 function PlayerPanel(opts) {
     var self = {};
 
-    var kills = 0;
+    var _kills = 0;
 
     var _panel = $("#player-panel");
     var _hpPanel = _panel.find(".hp-panel");
-    var _hp = _hpPanel.find(".hp");
+    var _hpLoading = _hpPanel.find(".hp");
+    var _killsPanel = _panel.find(".kills-panel");
 
     self.initHandlers = function() {
         $(document).bind("player_hp_change", function(e, hp, maxHp) {
-            _hp.css({"width": 100 * hp / maxHp + "%"});
+            _hpLoading.css({"width": 100 * hp / maxHp + "%"});
         });
 
         $(document).bind("enemy_died", function(e) {
-            console.log(++kills);
+            _killsPanel.html(++_kills)
         });
     };
 
