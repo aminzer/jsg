9d314e4528a611f1abd910afd30b174daef97d22
Minzer_AS <minzer.alexey@gmail.com>
2015-10-02 15:00:27 +0300

    expl. rocket inheritance from exp. bullet

--- js/jsg/objects/weapons/bullets/explosive-bullet.js
+++ js/jsg/objects/weapons/bullets/explosive-bullet.js
@@ -3,9 +3,10 @@ function ExplosiveBullet(opts, render) {
 
     Bullet.call(this, opts, false);
 
-    this._explosionCount = this.def( opts.explosionCount, 3 );
+    this._explosionCount = this.def( opts.explosionCount, 2 );
     this._childCount = this.def( opts.childCount, 3 );
     this._sector = this.def( opts.sector, 10 );
+    this._childBulletConstructor = this.def( opts.childBulletConstructor, ExplosiveBullet);
 
     if (render !== false) {
         this.render();
@@ -18,17 +19,57 @@ ExplosiveBullet.prototype.render = function() {
     Painter.circle(this, 2, "#00f");
 };
 
+ExplosiveBullet.prototype.getChildBulletOpts = function() {
+    return {
+        x: this.getX(),
+        y: this.getY(),
+        lifeTime: this.getLifetime(),
+        speed: this.getSpeed(),
+        damage: this.getDamage(),
+        explosionCount: this._explosionCount - 1
+    };
+};
+
 ExplosiveBullet.prototype.destroy = function() {
     if (this._explosionCount > 0) {
         for (var angle = this.getAngle() - this._sector/2; angle <= this.getAngle() + this._sector/2; angle += this._sector / (this._childCount-1)) {
-            gctx.addBullet(new ExplosiveBullet({
-                x: this.getX(),
-                y: this.getY(),
-                angle: angle,
-                lifeTime: this.getLifetime(),
-                speed: this.getSpeed(),
-                explosionCount: this._explosionCount - 1
-            }));
+            var childBullet = new this._childBulletConstructor(
+                this.getChildBulletOpts()
+            );
+            childBullet.setAngle(angle);
+            gctx.addBullet(childBullet);
         }
     }
+};
+
+ExplosiveBullet.prototype.getExplosionCount = function() {
+    return this._explosionCount;
+};
+
+ExplosiveBullet.prototype.setExplosionCount = function(explosionCount) {
+    this._explosionCount = explosionCount;
+};
+
+ExplosiveBullet.prototype.getChildCount = function() {
+    return this._childCount;
+};
+
+ExplosiveBullet.prototype.setChildCount = function(childCount) {
+    this._childCount = childCount;
+};
+
+ExplosiveBullet.prototype.getSector = function() {
+    return this._sector;
+};
+
+ExplosiveBullet.prototype.setSector = function(sector) {
+    this._sector = sector;
+};
+
+ExplosiveBullet.prototype.getChildBulletConstructor = function() {
+    return this._childBulletConstructor;
+};
+
+ExplosiveBullet.prototype.setChildBulletConstructor = function(bulletConstructor) {
+    this._childBulletConstructor = bulletConstructor;
 };
--- js/jsg/objects/weapons/bullets/explosive-rocket.js
+++ js/jsg/objects/weapons/bullets/explosive-rocket.js
@@ -1,24 +1,29 @@
 function ExplosiveRocket(opts, render) {
     opts = opts || {};
 
-    Rocket.call(this, opts, render);
+    ExplosiveBullet.call(this, opts, render);
 
     this.setDamage(this.def( opts.damage, BULLET.EXPLOSIVE_ROCKET.DAMAGE ));
     this.setSpeed(this.def( opts.speed, BULLET.EXPLOSIVE_ROCKET.START_SPEED ));
     this.setLifetime(this.def( opts.lifetime, BULLET.EXPLOSIVE_ROCKET.LIFETIME ));
+    this.setChildBulletConstructor(this.def( opts.childBulletConstructor, Fraction ));
+    this.setChildCount(this.def( opts.childCount, BULLET.EXPLOSIVE_ROCKET.FRACTION_COUNT ));
+    this.setSector(this.def (opts.sector, 360));
+
+    if (render !== false) {
+        this.render();
+    }
 }
 
-Extend(ExplosiveRocket).from(Rocket);
+Extend(ExplosiveRocket).from(ExplosiveBullet).withMixins(Rocket);
 
-ExplosiveRocket.prototype.destroy = function() {
-    for (var angle = 0; angle < 360; angle += 360 / BULLET.EXPLOSIVE_ROCKET.FRACTION_COUNT) {
-        gctx.addBullet(new Fraction({
-            x: this.getX() - 30 * cos_d(this.getAngle()),   // rocket explode in it's tail
-            y: this.getY() - 30 * sin_d(this.getAngle()),
-            angle: angle,
-            damage: BULLET.EXPLOSIVE_ROCKET.FRACTION_DAMAGE,
-            speed: 10 + random() * 5,
-            lifetime: BULLET.EXPLOSIVE_ROCKET.FRACTION_LIFETIME
-        }));
+ExplosiveRocket.prototype.getChildBulletOpts = function() {
+    return {
+        x: this.getX() - 30 * cos_d(this.getAngle()),   // rocket explode in it's tail
+        y: this.getY() - 30 * sin_d(this.getAngle()),
+        damage: BULLET.EXPLOSIVE_ROCKET.FRACTION_DAMAGE,
+        speed: 10 + random() * 5,
+        lifetime: BULLET.EXPLOSIVE_ROCKET.FRACTION_LIFETIME,
+        explosionCount: 2
     }
 };
