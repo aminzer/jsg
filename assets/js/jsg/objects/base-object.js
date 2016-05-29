108c254d86ae5349e249ce3a54e344647840f0fa
Minzer-Alexey <minzer.alexey@gmail.com>
2016-05-29 23:10:17 +0300

    use createjs.UID refactor RequestParams

--- assets/js/jsg/objects/base-object.js
+++ assets/js/jsg/objects/base-object.js
@@ -1,6 +1,6 @@
 function BaseObject(opts) {
     opts = opts || {};
-    this._id = meta.common.first_defined( opts.id, UID.generate() );
+    this._id = meta.common.first_defined( opts.id, createjs.UID.get() );
     this._objectType = meta.common.first_defined( opts.objectType, OBJECT_TYPE.UNDEFINED );
 }
 
