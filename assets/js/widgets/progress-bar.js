90744b7a2ef5789c73618f35bf1643099141418f
Minzer_AS <minzer.alexey@gmail.com>
2016-05-30 18:22:27 +0300

    fix progress bar animation

--- assets/js/widgets/progress-bar.js
+++ assets/js/widgets/progress-bar.js
@@ -21,7 +21,8 @@ function ProgressBar(opts) {
     function render() {
         $progressNode = $('<div class="progress-bar__progress"></div>').css({
             'width': toPercents(),
-            'background-color': opts.color
+            'background-color': opts.color,
+            'transition': getCssTransitionValue(opts.animationDuration)
         });
         $node = $('<div class="progress-bar"></div>')
             .css('border', opts.noBorder ? 'none' : '')
@@ -35,11 +36,18 @@ function ProgressBar(opts) {
     function setProgress(progress) {
         if (_progress != progress) {
             _progress = progress;
-            $progressNode.css('width', toPercents());  // TODO fix animation (jquery is not fine. it become shit, when setProgress calls interval is less, than animation time)
+            $progressNode.css('width', toPercents());
         }
     }
 
     function toPercents() {
         return 100 * _progress + '%';
+    }
+
+    function getCssTransitionValue(animationDuration) {
+        if (typeof animationDuration == 'undefined' || animationDuration) {
+            return 'width ' + (animationDuration || 100) + 'ms cubic-bezier(0.25, 0.1, 0.25, 1.0)';
+        }
+        return '';
     }
 }
